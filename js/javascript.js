13b9ffe41d4bc4690cd399001507b79c62fa1860
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-06-30 21:01:09 -0700

    added javascript code this will begin process for project requirements

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,6 @@
 window.onload = function () {
+    function beepBoop(number) {
+        const outputList = [];
+
+        for (let i = 0; i <= number; i++) {
+            let outputValue = i.toString();
