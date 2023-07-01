19e6b6f3a03717e288fe768f66826a83162c918c
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-06-30 21:20:09 -0700

    added more script code to javascript this will make code more complete

--- js/javascript.js
+++ js/javascript.js
@@ -4,3 +4,10 @@ window.onload = function () {
 
         for (let i = 0; i <= number; i++) {
             let outputValue = i.toString();
+            if (outputValue.includes("3")) {
+                outputValue = "Won't you be my neighbor?";
+            } else if (outputValue.includes("2")) {
+                outputValue = "Boop!";
+            } else if (outputValue.includes("1")) {
+                outputValue = "Beep!";
+            }
