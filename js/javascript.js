9b2c8e8bf64af4ae2b361ffe56d779830d203699
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-07-01 21:04:12 -0700

    fixed js and html code this will let code execute correctly

--- js/javascript.js
+++ js/javascript.js
@@ -1,27 +1,25 @@
-window.onload = function () {
-    function beepBoop(number) {
-        const outputList = [];
+function beepBoop(number) {
+    const outputList = [];
 
-        for (let i = 0; i <= number; i++) {
-            let outputValue = i.toString();
-            if (outputValue.includes("3")) {
-                outputValue = "Won't you be my neighbor?";
-            } else if (outputValue.includes("2")) {
-                outputValue = "Boop!";
-            } else if (outputValue.includes("1")) {
-                outputValue = "Beep!";
-            }
-
-            outputList.push(outputValue);
+    for (let i = 0; i <= number; i++) {
+        let outputValue = i.toString();
+        if (outputValue.includes("3")) {
+            outputValue = "Won't you be my neighbor?";
+        } else if (outputValue.includes("2")) {
+            outputValue = "Boop!";
+        } else if (outputValue.includes("1")) {
+            outputValue = "Beep!";
         }
 
-        return outputList;
+        outputList.push(outputValue);
     }
 
-    function generateOutput() {
-        const userInput = document.getElementById("userInput").value;
-        const outputElement = document.getElementById("output");
-        const outputList = beepBoop(parseInt(userInput, 10));
-        outputElement.textContent = outputList.join(", ");
-      }
-      
+    return outputList;
+}
+
+function generateOutput() {
+    const userInput = document.getElementById("userInput").value;
+    const outputElement = document.getElementById("output");
+    const outputList = beepBoop(parseInt(userInput, 10));
+    outputElement.textContent = outputList.join(", ");
+}
